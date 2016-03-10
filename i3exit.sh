4705e9b1638b707c4fcfbd15ad98f2a03b46f733
Axel Schaefer <axel.schaefer@wheregroup.com>
2016-03-10 18:41:39 +0100

    dbus-send

--- i3exit.sh
+++ i3exit.sh
@@ -28,7 +28,8 @@ case "$1" in
         systemctl reboot
         ;;
     shutdown)
-        systemctl poweroff
+        # systemctl poweroff
+        dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.PowerOff" boolean:true
         ;;
     *)
         echo "Usage: $0 {lock|logout|suspend|hibernate|reboot|shutdown}"
